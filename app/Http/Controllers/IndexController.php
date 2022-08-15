db83a33e82d95d93bdefdab09a2aa29ba012f72f
Aleksey <badproper98@gmail.com>
2022-08-15 12:51:57 +0300

    передал данные для вывода в шаблоне

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view("home.index");
+        $products = Product::all();
+        return view("home.index",['products'=>$products]);
     }
 }
